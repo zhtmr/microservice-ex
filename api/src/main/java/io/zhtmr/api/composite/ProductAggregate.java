654d85119dc9a0d5b7471a3b9f88c9c55d39afca
csw <susu199037@gmail.com>
2024-10-02 23:46:59 +0900

    feat: composite service

---
+++ api/src/main/java/io/zhtmr/api/composite/ProductAggregate.java
@@ -0,0 +1,47 @@
+package io.zhtmr.api.composite;
+
+import java.util.List;
+
+public class ProductAggregate {
+  private final int productId;
+  private final String name;
+  private final int weight;
+  private final List<RecommendationSummary> recommendations;
+  private final List<ReviewSummary> reviews;
+  private final ServiceAddresses serviceAddress;
+
+  public ProductAggregate(int productId, String name, int weight,
+      List<RecommendationSummary> recommendations, List<ReviewSummary> reviews,
+      ServiceAddresses serviceAddress) {
+    this.productId = productId;
+    this.name = name;
+    this.weight = weight;
+    this.recommendations = recommendations;
+    this.reviews = reviews;
+    this.serviceAddress = serviceAddress;
+  }
+
+  public int getProductId() {
+    return productId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getWeight() {
+    return weight;
+  }
+
+  public List<RecommendationSummary> getRecommendations() {
+    return recommendations;
+  }
+
+  public List<ReviewSummary> getReviews() {
+    return reviews;
+  }
+
+  public ServiceAddresses getServiceAddress() {
+    return serviceAddress;
+  }
+}
---
+++ api/src/main/java/io/zhtmr/api/composite/ProductCompositeService.java
@@ -0,0 +1,9 @@
+package io.zhtmr.api.composite;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public interface ProductCompositeService {
+  @GetMapping(value = "/product-composite/{productId}", produces = "application/json")
+  ProductAggregate getProduct(@PathVariable int productId);
+}
