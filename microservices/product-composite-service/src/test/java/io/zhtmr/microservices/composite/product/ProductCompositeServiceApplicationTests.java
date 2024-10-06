22d452ababbbdbb022177b404012d63d888f327d
csw <susu199037@gmail.com>
2024-10-06 20:36:37 +0900

    test: api test

--- microservices/product-composite-service/src/test/java/io/zhtmr/microservices/composite/product/ProductCompositeServiceApplicationTests.java
+++ microservices/product-composite-service/src/test/java/io/zhtmr/microservices/composite/product/ProductCompositeServiceApplicationTests.java
@@ -1,11 +1,72 @@
 package io.zhtmr.microservices.composite.product;
 
+import io.zhtmr.api.core.product.Product;
+import io.zhtmr.api.core.recommendation.Recommendation;
+import io.zhtmr.api.core.review.Review;
+import io.zhtmr.api.exceptions.NotFoundException;
+import io.zhtmr.microservices.composite.product.services.ProductCompositeIntegration;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static java.util.Collections.singletonList;
+import static org.mockito.Mockito.when;
+
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductCompositeServiceApplicationTests {
+	private static final int PRODUCT_ID_OK = 1;
+	private static final int PRODUCT_ID_NOT_FOUND = 2;
+	private static final int PRODUCT_ID_INVALID = 3;
 
+	@Autowired
+	private WebTestClient client;
+
+	@MockBean
+	private ProductCompositeIntegration compositeIntegration;
+
+	@BeforeEach
+	void setUp() {
+		when(compositeIntegration.getProduct(PRODUCT_ID_OK))
+				.thenReturn(new Product(PRODUCT_ID_OK, "name", 1, "mock-address"));
+		when(compositeIntegration.getRecommendations(PRODUCT_ID_OK))
+				.thenReturn(singletonList(new Recommendation(PRODUCT_ID_OK, 1, "author", 1, "content", "mock address")));
+		when(compositeIntegration.getReviews(PRODUCT_ID_OK))
+				.thenReturn(singletonList(new Review(PRODUCT_ID_OK, 1, "author", "subject", "content", "mock address")));
+
+		when(compositeIntegration.getProduct(PRODUCT_ID_NOT_FOUND))
+				.thenThrow(new NotFoundException("NOT FOUND: " + PRODUCT_ID_NOT_FOUND));
+	}
+
+
+	@Test
+	void getProductById() {
+		client.get().uri("product-composite/" + PRODUCT_ID_OK)
+				.accept(MediaType.APPLICATION_JSON_UTF8)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+				.expectBody()
+				.jsonPath("$.productId").isEqualTo(PRODUCT_ID_OK)
+				.jsonPath("$.recommendations.length()").isEqualTo(1).jsonPath("$.reviews.length()")
+				.isEqualTo(1);
+	}
+
+	@Test
+	void getProductNotFound() {
+		client.get().uri("/product-composite/" + PRODUCT_ID_NOT_FOUND)
+				.accept(MediaType.APPLICATION_JSON_UTF8)
+				.exchange().expectStatus().isNotFound()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+				.expectBody()
+				.jsonPath("$.path").isEqualTo("/product-composite/" + PRODUCT_ID_NOT_FOUND)
+				.jsonPath("$.message").isEqualTo("NOT FOUND: " + PRODUCT_ID_NOT_FOUND);
+	}
 	@Test
 	void contextLoads() {
 	}
